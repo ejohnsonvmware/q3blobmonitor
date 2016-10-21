3fea91b758582adcadbedef234863547115ddc11
Erik Johnson <ejohnson@vmware.com>
2016-10-20 20:51:40 -0600

    Add blob monitor code to script.

--- blobmonitor.sh
+++ blobmonitor.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+url="http://blobs.vmwaredevops.appspot.com/api/v1/blobs/10"
+errorurl="http://localhost:18081/error"
+content=`curl -s ${url} | python -c "import sys, json; print json.load(sys.stdin)['content']"`
+origmd5=`echo ${content} | md5sum`
+
+echo "Orignal MD5: $origmd5"
+
+while [ 1 ]
+do
+
+  content=`curl -s ${url} | python -c "import sys, json; print json.load(sys.stdin)['content']"`
+  md5=`echo ${content} | md5sum`
+
+
+  if [ "${origmd5}" != "${md5}" ]
+  then
+    echo "Blob changed"
+    echo "[DEBUG] start=$origmd5 current=$md5"
+    curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 2184b222-1cb8-9669-ed6a-1ed4750b31e8" -d '{ "service": "postman","message": "Blob file changed" }' ${errorurl}
+  else
+    echo "[DEBUG] md5=$md5"
+
+  fi
+  sleep 5 
+done
